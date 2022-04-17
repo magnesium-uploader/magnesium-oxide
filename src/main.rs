8e6963eb6ee5c2ebc207bdfdfd642fc362ccab1b
Checksum <24845326+ChecksumDev@users.noreply.github.com>
2022-04-16 22:22:51 -0500

    refactor: :art: Reformat and remove the user_delete endpoint.

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
 use actix_web::{web, App, HttpServer};
-use mongodb::{Database};
+use mongodb::Database;
 
-mod log;
-mod routes;
 mod config;
 mod crypto;
+mod log;
+mod routes;
 mod startup;
 
 use config::Config;
@@ -20,20 +20,32 @@ async fn main() -> std::io::Result<()> {
     let appstate: AppState = startup::startup().await;
     let _appstate = appstate.clone();
     if let Ok(o) = HttpServer::new(move || {
-            App::new().app_data(web::Data::new(_appstate.clone())).service(routes::v1::files::get_file).service(routes::v1::files::delete_file).service(
+        App::new()
+            .app_data(web::Data::new(_appstate.clone()))
+            .service(routes::v1::files::get_file)
+            .service(routes::v1::files::delete_file)
+            .service(
                 web::scope("/api").service(
-                    web::scope("/v1").service(
-                        web::scope("/users").route("", web::post().to(routes::v1::users::create)).route("", web::delete().to(routes::v1::users::delete)),
-                    ).service(
-                        web::scope("/files").route("", web::post().to(routes::v1::files::upload)),
-                    ),
+                    web::scope("/v1")
+                        .service(
+                            web::scope("/users")
+                                .route("", web::post().to(routes::v1::users::create)),
+                        )
+                        .service(
+                            web::scope("/files")
+                                .route("", web::post().to(routes::v1::files::upload)),
+                        ),
                 ),
             )
-        }).bind((&(*appstate.config.network.bind_address.to_string().as_str()), appstate.config.network.bind_port)) {
+    })
+    .bind((
+        &(*appstate.config.network.bind_address.to_string().as_str()),
+        appstate.config.network.bind_port,
+    )) {
         log::info("Server started");
         o.run().await
     } else {
-                log::critical("Could not bind to address");
-                std::process::exit(1);
-            }
+        log::critical("Could not bind to address");
+        std::process::exit(1);
+    }
 }
