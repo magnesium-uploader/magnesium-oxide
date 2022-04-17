de86457a3aa538d30a14ce95155502f792a53912
Checksum <24845326+ChecksumDev@users.noreply.github.com>
2022-04-16 22:22:01 -0500

    fix: :technologist: Use GENERICS for logging!
    This allows us to use `log::debug(1)` instead of `log::debug("1")`, among other things.

--- src/log.rs
+++ src/log.rs
@@ -1,29 +1,25 @@
-#[allow(dead_code)]
+#![allow(dead_code)]
+
 fn get_time() -> String {
     chrono::offset::Utc::now().to_rfc2822()
 }
 
-#[allow(dead_code)]
-pub fn debug(message: &str) {
-    println!("\x1b[90m[{}] DEBUG: {}\x1b[0m", get_time(), message);
+pub fn debug<T: std::fmt::Display>(t: T) {
+    println!("\x1b[90m[{}] DEBUG: {}\x1b[0m", get_time(), t);
 }
 
-#[allow(dead_code)]
-pub fn info(message: &str) {
-    println!("\x1b[1;34m[{}] INFO: {}\x1b[0m", get_time(), message);
+pub fn info<T: std::fmt::Display>(t: T) {
+    println!("\x1b[1;34m[{}] INFO: {}\x1b[0m", get_time(), t);
 }
 
-#[allow(dead_code)]
-pub fn warn(message: &str) {
-    println!("\x1b[1;33m[{}] WARN: {}\x1b[0m", get_time(), message);
+pub fn warn<T: std::fmt::Display>(t: T) {
+    println!("\x1b[1;33m[{}] WARN: {}\x1b[0m", get_time(), t);
 }
 
-#[allow(dead_code)]
-pub fn error(message: &str) {
-    println!("\x1b[1;31m[{}] ERROR: {}\x1b[0m", get_time(), message);
+pub fn error<T: std::fmt::Display>(t: T) {
+    println!("\x1b[1;31m[{}] ERROR: {}\x1b[0m", get_time(), t);
 }
 
-#[allow(dead_code)]
-pub fn critical(message: &str) {
-    println!("\x1b[1;35m[{}] CRITICAL: {}\x1b[0m", get_time(), message);
+pub fn critical<T: std::fmt::Display>(t: T) {
+    println!("\x1b[1;31m[{}] CRITICAL: {}\x1b[0m", get_time(), t);
 }
